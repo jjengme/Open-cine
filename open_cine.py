68ec10e3f84edb01a0639a53f8e58bc5b5da483d
황정민 <v02happy@naver.com>
2024-09-11 15:37:39 +0900

    Update open_cine.py
    Network change

--- open_cine.py
+++ open_cine.py
@@ -7,8 +7,8 @@ import cv2 as cv
 
 ph = Phantom() # cine 파일을 불러올 때 항상해줘야됨
 
-fname = 'Z:/03 exp/220126 ilasskorea/2bar 2x/2bar 106.cine'
-# fname = 'Z:/03 exp/투명노즐/32uLD.cine'
+#fname = 'Z:/03 exp/220126 ilasskorea/2bar 2x/2bar 106.cine'
+fname = '//203.253.185.128\kk/03 exp/240905/wi2.cine'
 frame = 0
 
 test = cine2nparr(fname, frame)
